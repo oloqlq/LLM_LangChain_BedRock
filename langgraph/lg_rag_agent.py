209964d0bccf01c6d310151541c11fca230feebe
oloqlq <oloqlqxown0919@gmail.com>
2026-06-02 10:09:21 +0900

    Feat : 최종 연결 완료

--- langgraph/lg_rag_agent.py
+++ langgraph/lg_rag_agent.py
@@ -29,7 +29,7 @@ load_dotenv()
 llm = ChatBedrockConverse(model       = os.getenv('MODEL_ID'), 
                           max_tokens  = 1000,
                           temperature = 0.5,
-                          region_name = os.getnv('AWS_REGION'))
+                          region_name = os.getenv('AWS_REGION'))
 
 tools = [rag_search]
 llm_with_tools = llm.bind_tools(tools)
@@ -43,13 +43,13 @@ examples        = [
     {"input":"다이어트를 위해서 칼로리가 낮은 메뉴로", "output":"관리하시는군요. 닭가슴살 샐러드 드세요."}
 ]
 
-example_prompt  = ChatPromptTemplate.from_messages(
-    ('human', '{input}'),
-    ('ai', {'output'}),
-)
+example_prompt  = ChatPromptTemplate.from_messages([
+    ('human', '{input}'  ),
+    ('ai',    '{output}' ),
+])
 
 few_shot_prompt = FewShotChatMessagePromptTemplate(
-    examples = examples,
+    examples    = examples,
     example_prompt = example_prompt
 )
 
@@ -57,11 +57,10 @@ final_prompt    = ChatPromptTemplate.from_messages([
     # 1. Persona
     ('system', '당신은 센스있는 식사 메뉴 추천 전문가입니다. 사용자의 상황에 맞춰서 메뉴룰 추천하고 필요하면 도구를 사용하여 실제 식당을 찾으세요.'), 
     # 2. Few-Shot Sample
-    few_shot_prompt
+    few_shot_prompt,
     # 3. User Question
     ('human', '{query}')
 ])
-
 
 
 #---------------------------------------
@@ -80,7 +79,28 @@ def thinking_node( state:AgentState ):
     return {'messages':[ res ]}
 
 def tool_node( state:AgentState ):
-    pass
+    last_msg = state['messages'][-1]
+    print('tool_node 호출 : 툴 사용 LLM이 응답', last_msg.tool_calls)
+    if last_msg.tool_calls:
+        tool = last_msg.tool_calls[0]
+        '''
+        last_msg.tool_calls[
+            {
+                'name':'rag_search',
+                'args':{'cate':'가벼운 식사'},
+                'id':'toolsuse-aaldkjfaefb',
+                'type':'tool_call'            
+            }
+        ]
+        '''
+
+        result = rag_search.invoke(tool['args'])
+        print("RAG 호출 결과 : ", result)
+        return {"messages":[
+            HumanMessage(content=f'[사내데이터 검색결과]: {result}\n제공된 정보를 기반으로 최종 답변을 해줘.')
+        ]}
+    #보험용 return코드
+    return {"messages":[]}
 
 def final_answer_node( state:AgentState ):
     msg = state['messages']
@@ -88,31 +108,29 @@ def final_answer_node( state:AgentState ):
     return {'messages':[ res ]}
     pass
 
-
 # langgraph 연결
 workflow = StateGraph(AgentState)
 workflow.add_node('thinking',       thinking_node)
-
-# 조건부 엣지
-'''
 workflow.add_node('tool',           tool_node)
 workflow.add_node('final_answer',   final_answer_node)
-workflow.set_entry_point(thinking_node) 
+workflow.set_entry_point('thinking') 
 
 def custom_check_tool_node(state:AgentState):
-    pass
+    last_msg = state['messages'][-1]
+    if last_msg.tool_calls:
+        print('툴 사용 필요')
+        return 'tool'
+    return END
+
 workflow.add_conditional_edges('thinking', custom_check_tool_node)
 workflow.add_edge('tool', 'final_answer')
 workflow.add_edge('final_answer', END)
-'''
 
 #---------------------------------------
 # 
 #---------------------------------------
 
-workflow.add_edge("thinking", END)
 랭그래프객체 = workflow.compile()
-
 if __name__ == '__main__':
     res = 랭그래프객체.invoke( {"messages": "가벼운 식사"} )
     print( res )
