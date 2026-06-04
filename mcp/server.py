15f5047530520450493579891d1e1d727d0d3940
oloqlq <oloqlqxown0919@gmail.com>
2026-06-04 10:18:23 +0900

    Feat : 로깅 설정, add Tool 구현

--- mcp/server.py
+++ mcp/server.py
@@ -18,6 +18,13 @@ from mcp.server.fastmcp import FastMCP
 # Logging 설정
 #--------------------------------
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='[MCP Server] %(levelname)s: %(message)s',
+    stream=sys.stderr
+)
+logger = logging.getLogger(__name__)
+logger.info('MCP Server 구성 중 ...')
 
 
 
@@ -26,26 +33,37 @@ from mcp.server.fastmcp import FastMCP
 # MCP 서버 설정 
 #--------------------------------
 
+mcp = FastMCP('6ToolsMCPServer')
+logger.info('MCPServer 구성(초기화) 중 ...')
 
 
 
 #--------------------------------
 # 인메모리
 #--------------------------------
-
+note_memory = dict()
 
 
 
 #--------------------------------
 # Tool 구현 
 #--------------------------------
-
 # 외부 리소스
-
 # 간단한 기능 구현 (6개)
 
-
-
+def add(a:float, b:float) -> str:
+    '''
+    두 수를 더하는 계산기
+    Args : 
+        a : 첫 번째 수치
+        b : 두 번째 수치
+    
+    Returns
+        계산 결과
+    '''
+    result = a + b
+    logger.info(f'Tool 1 add 호출: {a} + {b} = {result}')
+    return f'계산 결과 : {a}+{b}={result}'
 
 #--------------------------------
 # 서버 가동
