5815fd4724093dc34815ca859b3ae512d9d6c79e
oloqlq <oloqlqxown0919@gmail.com>
2026-06-04 14:34:34 +0900

    Feat : llm langgraph mcp 기본 연동 구조

--- mcp/bedrock_mcp_agent.py
+++ mcp/bedrock_mcp_agent.py
@@ -4,3 +4,25 @@ Agent 개발
     - Bedrock 기반 llm 사용
     - MCP를 이용하여 tool 사용
 '''
+
+
+#-------------------------------------
+# Package Import
+#-------------------------------------
+import os
+import boto3
+import asyncio
+from dotenv import load_dotenv
+
+from langchain_aws import ChatBedrock
+from langchain_core.messages import HumanMessage
+from langchain_core.tools import BaseTool
+from langgraph.graph import StateGraph, END, MessagesState
+from langgraph.prebuilt import ToolNode
+
+from mcp_tools_adapter import MCPClient
+
+
+#-------------------------------------
+# 
+#-------------------------------------
