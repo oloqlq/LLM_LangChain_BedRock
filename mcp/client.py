7cd2b263309c8566b6e1662dd1677eec8ccc8c88
oloqlq <oloqlqxown0919@gmail.com>
2026-06-04 14:03:56 +0900

    Feat : 도구 호출 & 결과 확인

--- mcp/client.py
+++ mcp/client.py
@@ -1,5 +1,6 @@
 '''
 MCP Client
+    - 구동시 관련 파일 외에는 프로젝트 폴더에 없어야 함 => 실행하는 루트 디렉토리를 클리어 하게 사용
 MCP Server 와 통신
 '''
 # 1. 모듈 가져오기
@@ -37,14 +38,44 @@ class MCPClient:
             async with stdio_client(server_params) as (read, write):
                 async with ClientSession(read, write) as session:
                     print(f'MCP 서버 연결 성공 : 서버측으로부터 입력, 출력에 대한 객체 획득')
+                    # 1. 세션 초기화
                     await session.initialize()
+                    # 2. 사용 가능한 모든 도구 조회
                     print(f'MCP 서버측에 도구 목록 요청')
                     res = await session.list_tools()
                     self.tools = res.tools
-                    print(f'{len(self.tools)}개의 도구가 확인됨.')
+                    print(f'{len(self.tools)}개의 도구 확인됨.')#, self.tools[0])
+                    for i, tool in enumerate(self.tools, 1):
+                        print( f'   {i}. {tool.name}')
+                        print( f'   설명: {tool.description}')
+                        # 파라미터(매개변수) 정보 추출 => 스키마 => 매개변수명, 타입
+                        if hasattr(tool, "inputSchema") and tool.inputSchema: # 해당맴버가 있는가? -> 값은 있는가?
+                            props = tool.inputSchema.get('properties', {}) # 키->값 추출 (인덱싱), 비워있는 경우 대비 초기값 부여
+                            if props:
+                                print( f'   입력: { ", ".join(props.keys()) } ') # a, b
+
+                        print('-'*30 + '\n')
+
+                    print('\n'+'-'*30)
+                    print('도구 호출 테스트')
+                    print('-'*30 + '\n')
+                    await self.call_tool( session, "add", {"a":100, "b":5})                    
+
 
         except Exception as e:
             print( f'MCP Server 접속 오류 : {e}' )
+        pass
+    
+    # MCP 서버에 존재하는 도구를 호출하는 함수
+    async def call_tool(self, session, tool_name: str, arguments: dict):
+        try:
+            result = await session.call_tool(tool_name, arguments)
+            # 결과 출력
+            print(f'결과출력 : {result}')
+            return result
+        except Exception as e:
+            print( f'에러 발생 {e}' )
+            raise
         pass
 
 # 3. 비동기 main 함수 구성
