e4729e77d2a7e929292d3909b67c6d64e69a1637
oloqlq <oloqlqxown0919@gmail.com>
2026-06-05 10:18:01 +0900

    Feat : 세션 툴 목록 획득

--- mcp/mcp_tools_adapter.py
+++ mcp/mcp_tools_adapter.py
@@ -38,31 +38,49 @@ class MCPToolAdapter:
         # 메세지가 오염되면 => 출력을 sys.stderr
         print('MCP 서버 연결중..')
         try:
+            # 입력, 출력, 세션등, 여러 함수에서 사용한다면 with x
+            # MCP 서버를 접속할때 사용하는 내부 컨텍스트 객체
             self._stdio_context = stdio_client(server_params)
+            # 내부 함수를 강제로 호출(내가 원하는 시점에 처리)
+            # 비동기, 입력/출력 스트림 연결하여 반환
             stdio_tuple = await self._stdio_context.__aenter__()
-            if isinstance( stdio_tuple, tuple):
+            # 입력/출력 스트 획득
+            if isinstance( stdio_tuple, tuple ):
                 self.read_stream, self.write_stream = stdio_tuple
             else:
-                self.read_stream    = stdio_tuple
-                self.write_stream   = stdio_tuple
+                self.read_stream  = stdio_tuple
+                self.write_stream = stdio_tuple
+
+            # 세션 획득(생성) => 툴을 가져올수 있음
+            # JSON RPC 2.0 기준 상호 통신할수 있는 논리적인 상태(세션) 완성
+            self.session = ClientSession(self.read_stream, self.write_stream)
+            # 실제 활성화를 위해 직접 호출
+            await self.session.__aenter__()
+            # 세션 초기화
+            await self.session.initialize()
+            # MCP 서버에게 Tool 목록 요청
+            res = await self.session.list_tools()
+            self.mcp_tools = res.tools
+            print(f'MCP 서버로부터 { len(self.mcp_tools) }개의 툴 로드됨')
             
+            return self
         except Exception as e:
             print('MCP 서버 연결 실패', e)
             raise
         pass
     
     async def cleanup(self):
-        '''입/출력 스트림, 세션 등 자원 해제'''
+        '''입력/출력 스트림, 세션등 자원 해제(개발자 관리)'''
+        # 세션이 존재하면 -> 세션 종료
+        # 컨텍스트가 존재하면 -> 입력/출력 스트림 종료
         try:
             if self._stdio_context:
                 await self._stdio_context.__aexit__(None, None, None)
         except Exception as e:
-            print('입/출력 스트림 종료 에러', e)
+            print('입력/출력 스트림 종료 에러', e)
         pass
-
-
-
 
 # 4. 테스트
 if __name__ == '__main__':
     pass
+
